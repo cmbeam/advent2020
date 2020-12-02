2508d957c4e894b2c108ce7b3f095d7b2fab9a52
Chris Beam <chris_beam_16@yahoo.com>
2020-12-02 13:54:07 -0500

    Day 1 clean up

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -1,59 +1,78 @@
 from itertools import combinations
 import time
 
-puzzleInput = []
-file = open('Day1/day1.txt', 'r')
-for entry in file:
-    puzzleInput.append(int(entry))
+
+def loadpuzzle(file):
+    puzzleInput = []
+    file = open('Day1/day1.txt', 'r')
+    for entry in file:
+        puzzleInput.append(int(entry))
+    return puzzleInput
+
+
+def combo2(input):
+    answer = 0
+    start = time.perf_counter()
+    perm = combinations(input, 2)
+    for i in list(perm):
+        sum = i[0] + i[1]
+        if sum == 2020:
+            answer = i[0] * i[1]
+    stop = time.perf_counter()
+    # print(f"Time: {stop - start:0.4f}")
+    return answer
+
+
+def bruteforce2(input):
+    answer = 0
+    start = time.perf_counter()
+    for x in range(0, len(input)):
+        for y in range(x+1, len(input)):
+            sum = input[x] + input[y]
+            if sum == 2020:
+                answer = input[x] * input[y]
+
+    stop = time.perf_counter()
+    # print(f"Time: {stop - start:0.4f}")
+    return answer
+
+
+def combo3(input):
+    answer = 0
+    start = time.perf_counter()
+    perm = combinations(input, 3)
+    for i in list(perm):
+        sum = i[0] + i[1] + i[2]
+        if sum == 2020:
+            answer = i[0] * i[1] * i[2]
+
+    stop = time.perf_counter()
+    # print(f"Time: {stop - start:0.4f}")
+    return answer
+
+
+def bruteforce3(input):
+    answer = 0
+    start = time.perf_counter()
+    for x in range(0, len(input)):
+        for y in range(x+1, len(input)):
+            for z in range(0, len(input)):
+                if z != x & z != y:
+                    sum = input[x] + input[y] + input[z]
+                    if sum == 2020:
+                        answer = input[x] * input[y] * input[z]
+    stop = time.perf_counter()
+    # print(f"Time: {stop - start:0.4f}")
+    return answer
+
+
+# Load puzzle file
+puzzleInput = loadpuzzle("Day1/day1.txt")
 
 # Part 1
-answer1 = 0
-
-start = time.perf_counter()
-perm = combinations(puzzleInput, 2)
-for i in list(perm):
-    sum = i[0] + i[1]
-    if sum == 2020:
-        answer1 = i[0] * i[1]
-print(answer1)
-stop = time.perf_counter()
-print(f"Time: {stop - start:0.4f}")
-
-
-start = time.perf_counter()
-for x in range(0, len(puzzleInput)):
-    for y in range(x+1, len(puzzleInput)):
-        sum = puzzleInput[x] + puzzleInput[y]
-        if sum == 2020:
-            answer1 = puzzleInput[x] * puzzleInput[y]
-
-print(answer1)
-stop = time.perf_counter()
-print(f"Time: {stop - start:0.4f}")
+print("Part 1 with combinations: " + str(combo2(puzzleInput)))
+print("Part 1 with brute force: " + str(bruteforce2(puzzleInput)))
 
 # Part 2
-answer2 = 0
-
-start = time.perf_counter()
-perm = combinations(puzzleInput, 3)
-for i in list(perm):
-    sum = i[0] + i[1] + i[2]
-    if sum == 2020:
-        answer2 = i[0] * i[1] * i[2]
-
-print(answer2)
-stop = time.perf_counter()
-print(f"Time: {stop - start:0.4f}")
-
-start = time.perf_counter()
-for x in range(0, len(puzzleInput)):
-    for y in range(x+1, len(puzzleInput)):
-        for z in range(0, len(puzzleInput)):
-            if z != x & z != y:
-                sum = puzzleInput[x] + puzzleInput[y] + puzzleInput[z]
-                if sum == 2020:
-                    answer2 = puzzleInput[x] * puzzleInput[y] * puzzleInput[z]
-
-print(answer2)
-stop = time.perf_counter()
-print(f"Time: {stop - start:0.4f}")
+print("Part 2 with combinations: " + str(combo3(puzzleInput)))
+print("Part 2 with brute force: " + str(bruteforce3(puzzleInput)))
