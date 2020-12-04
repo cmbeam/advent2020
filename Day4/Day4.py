8fd9b7dbb525e341a3d3c838767e5fedfe5674c0
Chris Beam <chris_beam_16@yahoo.com>
2020-12-04 09:43:48 -0500

    Day 4 some clean up

--- Day4/Day4.py
+++ Day4/Day4.py
@@ -31,7 +31,7 @@ def validate(file, strict):
                     if int(passport['eyr']) > 2030:
                         valid = 1
 
-                    if not re.match('.*cm|.*in', passport['hgt']):
+                    if not re.match('(.*cm|.*in)$', passport['hgt']):
                         valid = 1
                     if re.match('.*cm', passport['hgt']):
                         height = passport['hgt'].strip('cm')
@@ -42,17 +42,14 @@ def validate(file, strict):
                         if 76 > int(height) < 59:
                             valid = 1
 
-                    if not re.match('#[0-9a-f]{6}', passport['hcl']):
+                    if not re.match('#[0-9a-f]{6}$', passport['hcl']):
                         valid = 1
-                    if not re.match('amb|blu|brn|gry|grn|hzl|oth', passport['ecl']):
+                    if not re.match('amb|blu|brn|gry|grn|hzl|oth$', passport['ecl']):
                         valid = 1
-                    if not re.match('[0-9]{9}', passport['pid']):
+                    if not re.match('[0-9]{9}$', passport['pid']):
                         valid = 1
-                    if re.match('[0-9]{10}', passport['pid']):
-                        valid = 1
-                    if strict == True and valid == 0:
+                    if strict and valid == 0:
                         count = count + 1
-                        # print(str(passport['byr']) + "    " + str(passport['iyr'] )+ "    " + str(passport['eyr'])+ "    " + str(passport['pid']) + "    " + str(passport['hcl'])+ "    " + str(passport['ecl'])+ "    " + str(passport['hgt']))
                     if not strict:
                         count = count + 1
                 passport = {}
@@ -72,9 +69,9 @@ def validate(file, strict):
 # Load file
 file = 'Day4/day4.txt'
 # Part 1
-count = validate(file, False)
-print(count)
+answer = validate(file, False)
+print(answer)
 
 # Part 2
-count = validate(file, True)
-print(count)
+answer = validate(file, True)
+print(answer)
