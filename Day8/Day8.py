ab3e90af34169bda286018488f2e5961b163235f
Chris Beam <chris_beam_16@yahoo.com>
2020-12-08 16:47:57 -0500

    Day 8 clean up

--- Day8/Day8.py
+++ Day8/Day8.py
@@ -1,41 +1,33 @@
-def loadpuzzle(file):
+class Instruction:
+    def __init__(self, operation, argument):
+        self.operation = operation
+        self.argument = argument
+
+
+def load(filename):
     instructions = {}
-    x = 0
-    with open(file) as puzzle:
-        for line in puzzle:
-            instruction = {}
-            operation = line.split(' ')[0]
-            argument = line.split(' ')[1]
-            number = argument[1:]
-            sign = argument[0]
-            instruction[0] = operation
-            instruction[1] = sign
-            instruction[2] = number
+    with open(filename) as file:
+        for x, line in enumerate(file):
+            instruction = Instruction(line[:3], int(line[4:]))
             instructions[x] = instruction
-            x = x + 1
     return instructions
 
 
-code = loadpuzzle("Day8/day8.txt")
+code = load("Day8/day8.txt")
 print(code)
-
 
 accumulator = 0
 marker = 0
 register = [0 for j in range(len(code))]
-run = True
 replacecount = -1
 counter = 0
 
-
-while marker < len(code) and run:
-    # print(str(marker + 1) + "  " + str(code[marker]))
+while marker < len(code):
     if register[marker] == 1:
         if replacecount == -1:
             print("Part 1 Answer: " + str(accumulator))
         else:
             print("Infinite loop.   Interupted  Accumulator: " + str(accumulator))
-        # run = False
         marker = 0
         accumulator = 0
         register = [0 for j in range(len(code))]
@@ -43,8 +35,7 @@ while marker < len(code) and run:
         counter = 0
     else:
         register[marker] = 1
-
-        operation = code[marker][0]
+        operation = code[marker].operation
         if operation == 'nop':
             if counter == replacecount:
                 operation = 'jmp'
@@ -52,23 +43,15 @@ while marker < len(code) and run:
                 marker = marker + 1
             counter = counter + 1
         if operation == 'acc':
-            sign = code[marker][1]
-            number = code[marker][2]
-            if sign == '+':
-                accumulator = accumulator + int(number)
-            else:
-                accumulator = accumulator - int(number)
+            number = code[marker].argument
+            accumulator = accumulator + int(number)
             marker = marker + 1
         if operation == 'jmp':
             if counter == replacecount:
                 marker = marker + 1
             else:
-                sign = code[marker][1]
-                number = code[marker][2]
-                if sign == '+':
-                    marker = marker + int(number)
-                else:
-                    marker = marker - int(number)
+                number = code[marker].argument
+                marker = marker + int(number)
             counter = counter + 1
 
 print("Part 2 Answer: " + str(accumulator))
