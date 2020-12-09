6e187b119620ae56354c8b5156d5f01e7f7184ee
Chris Beam <chris_beam_16@yahoo.com>
2020-12-09 08:39:15 -0500

    Day 9 part 2  extremely rough

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -11,7 +11,7 @@ def checkValid(block, number):
         x = int(x)
         for y in block[count + 1:]:
             y = int(y)
-            print("x: " + str(x) + " y: " + str(y) + " sum: " + str(x+y) + " Number: " + str(number))
+            # print("x: " + str(x) + " y: " + str(y) + " sum: " + str(x+y) + " Number: " + str(number))
             if x != y and x + y == number:
                 return True
         count += 1
@@ -25,11 +25,35 @@ def addToBlock(block, number):
     newBlock[24] = number
     return newBlock
 
+def findContiguous(block, number):
+    count = 0
 
+    for x in block:
+        x = int(x)
+        sum = x
+        smallest = x
+        largest = x
+        for y in block[count + 1:]:
+            y = int(y)
+            sum = sum + y
+            if y < smallest:
+                smallest = y
+            if y > largest:
+                largest = y
+            print(str(sum) + " " + number)
+            if sum == int(number):
+                print("done  smallest: " + str(smallest) + " largest: " + str(largest) + " sum: " + str(smallest + largest))
+
+                return True
+        count += 1
+    return False
 
 data = load("Day9/day9.txt")
 
-
+wholeblock = [0 for j in range(len(data))]
+for x in data:
+    wholeblock[x] = data[x]
+print(wholeblock)
 currentblock = [0 for j in range(25)]
 for x in data:
     if x < 25:
@@ -41,5 +65,6 @@ for x in data:
         print(currentblock)
         if not checkValid(currentblock, int(data[x])):
             print("First Invalid: " + str(data[x]))
+            findContiguous(wholeblock, data[x])
             break
         currentblock = addToBlock(currentblock, data[x])
