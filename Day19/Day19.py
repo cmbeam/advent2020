ce5490ee4b9034e8fc35462d6713b659dfb585df
Chris Beam <chris_beam_16@yahoo.com>
2020-12-19 12:53:05 -0500

    Day 19 part 2 done rough

--- Day19/Day19.py
+++ Day19/Day19.py
@@ -17,9 +17,6 @@ def load(filename):
     file.close()
     return rules, messages
 
-data = load("day19.txt")
-
-
 def getValidFromRule(rules, rule_number):
     validMessages = []
     #print(str(rule_number) + str(rules[rule_number][0]))
@@ -44,34 +41,29 @@ def getValidFromRule(rules, rule_number):
                 #print(str(rule_number) +" intra: " + str(combos))
             #print(combos)
             validMessages = validMessages + combos
-
-
-
-
-
-
-
     #print("messages: " + str(validMessages))
     return validMessages
 
-#validMessages = getValidFromRule(data[0], '0')
-#print(validMessages)
 
-# count = 0
-# for message in data[1]:
-#     #print(message)
-#     if message in validMessages:
-#         #print("valid")
-#         count += 1
-# print("Answer part 1: " + str(count))
+data = load("day19.txt")
+
+validMessages = getValidFromRule(data[0], '0')
+print(validMessages)
+
+count = 0
+for message in data[1]:
+    #print(message)
+    if message in validMessages:
+        #print("valid")
+        count += 1
+print("Answer part 1: " + str(count))
+print()
+print()
 
 
 # part 2
 fourty_two = validMessages = getValidFromRule(data[0], '42')
 thirty_one = validMessages = getValidFromRule(data[0], '31')
-
-#fourt_two = ['abbbabab', 'abbaabab', 'abababab', 'ababbaab', 'abbaaaab', 'abaaabbb', 'ababbbbb', 'abbbbbbb', 'abbbabbb', 'abbababb', 'ababaabb', 'abaaaabb', 'aaaaaabb', 'aabaaabb', 'aaabbabb', 'aabbbabb', 'aaababbb', 'aabbabbb', 'aaaaabbb', 'aaaabaab', 'aaabbaab', 'aaaaaaab', 'aabaaaab', 'aababaab', 'aabaabab', 'aabbabab', 'aababbab', 'aabbbbab', 'aaaabbab', 'aaabbbab', 'aaababab', 'bbbbabab', 'bbbbbbab', 'bbbbbaab', 'bbbabaab', 'bbbbaabb', 'bbbbbbbb', 'bbbaaabb', 'bbbaabbb', 'bbabaabb', 'bbababbb', 'bbabaaab', 'bbabbaab', 'bbabbabb', 'bbaabaab', 'bbaababb', 'bbaaabbb', 'bbaaabab', 'baaababb', 'baabbabb', 'baababbb', 'babbabbb', 'babababb', 'babaaabb', 'baaabaab', 'baabbbab', 'baabaaab', 'baabbaab', 'babaabab', 'babbbbab', 'aabbaaaa', 'abbbaaaa', 'aaabaaaa', 'abbaaaaa', 'aaaaaaaa', 'abaaaaaa', 'aaaabaaa', 'abaabaaa', 'aabbbaaa', 'abbbbaaa', 'bababaaa', 'babbbaaa', 'babbaaaa', 'baaaaaaa', 'baabbaaa', 'bbaabaaa', 'bbbbbaaa', 'bbabaaaa', 'baaabbaa', 'bababbaa', 'ababbbaa', 'bbabbbaa', 'baabbbaa', 'babbbbaa', 'abbbbbaa', 'baababaa', 'bbababaa', 'babbabaa', 'bbbbabaa', 'aabbabaa', 'abbbabaa', 'abababaa', 'abbaabaa', 'aaaaabaa', 'abaaabaa', 'baaaabaa', 'babaabaa', 'aabbbaba', 'aaaaaaba', 'aabaaaba', 'aaabaaba', 'aabbaaba', 'aabbabba', 'aabaabba', 'abbababa', 'abbbbaba', 'abbbaaba', 'abaaaaba', 'ababbaba', 'abbabbba', 'abbbbbba', 'abbbabba', 'abababba', 'bbbbbaba', 'bbabbaba', 'bbbababa', 'babaaaba', 'bbbaaaba', 'bbaaaaba', 'baabaaba', 'babbaaba', 'babbabba', 'baaaabba', 'babaabba', 'bbaaabba', 'bbababba', 'baabbbba', 'bababbba']
-
 
 print(fourty_two)
 reg42 = "("
@@ -89,26 +81,13 @@ reg31 = reg31 + thirty_one[-1]
 reg31 = reg31 + ')'
 print(reg31)
 
-#8 | 11
-#8 = 42 | 8
-#11: 42 31 | 42 11 31
-
+print("^("+reg42+"+)(("+reg42+"(?>z|(?1))*"+reg31+"))$")
 count = 0
 for message in data[1]:
-    # if regex.match("^"+reg42+"+$", message):
-    #      print("8 " +message)
-    #      count+=1
-    # if regex.match("^("+reg42+"(?>z|(?1))*"+reg31+")$", message):
-    #      print("11 " + message)
-    #      count+=1
     if regex.match("^"+reg42+"+("+reg42+"(?>z|(?2))*"+reg31+")$", message):
-        print("f " + message)
+        print(message)
         count+=1
 print("Answer part 2: " + str(count))
-print("^("+reg42+"+)(("+reg42+"(?>z|(?1))*"+reg31+"))$")
 
-matching = "trrrrxx"
-#\A(b(?>m|(?1))*e)\
-#^(t(?>[^()]|(?1))*x)$
-if regex.match('^(t|r)+(((t|r)(?>z|(?2))*(x|l)))$', matching):
-    print("Match_test")
+
+
