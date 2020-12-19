a5a7890f4520cd189ff532e95007b7444db87965
Chris Beam <chris_beam_16@yahoo.com>
2020-12-19 11:50:52 -0500

    Day 19 part 2 partial solution

--- Day19/Day19.py
+++ Day19/Day19.py
@@ -1,3 +1,5 @@
+import regex
+
 def load(filename):
     rules = {}
     messages = []
@@ -54,17 +56,56 @@ def getValidFromRule(rules, rule_number):
     #print("messages: " + str(validMessages))
     return validMessages
 
-validMessages = getValidFromRule(data[0], '0')
+#validMessages = getValidFromRule(data[0], '0')
 #print(validMessages)
 
-count = 0
-for message in data[1]:
-    #print(message)
-    if message in validMessages:
-        #print("valid")
-        count += 1
-print("Answer part 1: " + str(count))
+# count = 0
+# for message in data[1]:
+#     #print(message)
+#     if message in validMessages:
+#         #print("valid")
+#         count += 1
+# print("Answer part 1: " + str(count))
 
 
 # part 2
+fourty_two = validMessages = getValidFromRule(data[0], '42')
+thirty_one = validMessages = getValidFromRule(data[0], '31')
 
+#fourt_two = ['abbbabab', 'abbaabab', 'abababab', 'ababbaab', 'abbaaaab', 'abaaabbb', 'ababbbbb', 'abbbbbbb', 'abbbabbb', 'abbababb', 'ababaabb', 'abaaaabb', 'aaaaaabb', 'aabaaabb', 'aaabbabb', 'aabbbabb', 'aaababbb', 'aabbabbb', 'aaaaabbb', 'aaaabaab', 'aaabbaab', 'aaaaaaab', 'aabaaaab', 'aababaab', 'aabaabab', 'aabbabab', 'aababbab', 'aabbbbab', 'aaaabbab', 'aaabbbab', 'aaababab', 'bbbbabab', 'bbbbbbab', 'bbbbbaab', 'bbbabaab', 'bbbbaabb', 'bbbbbbbb', 'bbbaaabb', 'bbbaabbb', 'bbabaabb', 'bbababbb', 'bbabaaab', 'bbabbaab', 'bbabbabb', 'bbaabaab', 'bbaababb', 'bbaaabbb', 'bbaaabab', 'baaababb', 'baabbabb', 'baababbb', 'babbabbb', 'babababb', 'babaaabb', 'baaabaab', 'baabbbab', 'baabaaab', 'baabbaab', 'babaabab', 'babbbbab', 'aabbaaaa', 'abbbaaaa', 'aaabaaaa', 'abbaaaaa', 'aaaaaaaa', 'abaaaaaa', 'aaaabaaa', 'abaabaaa', 'aabbbaaa', 'abbbbaaa', 'bababaaa', 'babbbaaa', 'babbaaaa', 'baaaaaaa', 'baabbaaa', 'bbaabaaa', 'bbbbbaaa', 'bbabaaaa', 'baaabbaa', 'bababbaa', 'ababbbaa', 'bbabbbaa', 'baabbbaa', 'babbbbaa', 'abbbbbaa', 'baababaa', 'bbababaa', 'babbabaa', 'bbbbabaa', 'aabbabaa', 'abbbabaa', 'abababaa', 'abbaabaa', 'aaaaabaa', 'abaaabaa', 'baaaabaa', 'babaabaa', 'aabbbaba', 'aaaaaaba', 'aabaaaba', 'aaabaaba', 'aabbaaba', 'aabbabba', 'aabaabba', 'abbababa', 'abbbbaba', 'abbbaaba', 'abaaaaba', 'ababbaba', 'abbabbba', 'abbbbbba', 'abbbabba', 'abababba', 'bbbbbaba', 'bbabbaba', 'bbbababa', 'babaaaba', 'bbbaaaba', 'bbaaaaba', 'baabaaba', 'babbaaba', 'babbabba', 'baaaabba', 'babaabba', 'bbaaabba', 'bbababba', 'baabbbba', 'bababbba']
+
+
+print(fourty_two)
+reg42 = "("
+for exp42 in fourty_two[:-1]:
+    reg42 = reg42 + exp42 + '|'
+reg42 = reg42 + fourty_two[-1]
+reg42 = reg42 + ')'
+print(reg42)
+
+print(thirty_one)
+reg31 = "("
+for exp31 in thirty_one[:-1]:
+    reg31 = reg31 + exp31 + '|'
+reg31 = reg31 + thirty_one[-1]
+reg31 = reg31 + ')'
+print(reg31)
+
+#8 | 11
+#8 = 42 | 8
+#11: 42 31 | 42 11 31
+
+count = 0
+for message in data[1]:
+    if regex.match("^"+reg42+"+$", message):
+        count+=1
+    elif regex.match("^("+reg42+'(?>z|(?1))*'+reg31+")$", message):
+        print(message)
+        count+=1
+print("Answer part 2: " + str(count))
+
+matching = "tttxxx"
+#\A(b(?>m|(?1))*e)\
+#^(t(?>[^()]|(?1))*x)$
+if regex.match('^((t)(?>z|(?1))*(x))$', matching):
+    print("Match_test")
