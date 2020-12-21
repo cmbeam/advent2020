4e3d0e4a5d5f9d382c4c2de08a36ff50ed5c3214
Chris Beam <chris_beam_16@yahoo.com>
2020-12-20 19:11:35 -0500

    Day 20 Part 2 complete but needs cleanup

--- Day20/Day20.py
+++ Day20/Day20.py
@@ -20,6 +20,7 @@ def borders(grid):
 
 
 def rotate(tile):
+    print("rotate")
     grid = tile[1]
     new_grid = [[] for i in range(len(grid))]
     for line in reversed(grid):
@@ -43,11 +44,11 @@ def rotate(tile):
     return tile
 
 def flipY(tile):
-    print("flip: " + str(tile))
-    for line in tile[1]:
-        for c in line:
-            print(c, end='')
-        print()
+    # print("flip: " + str(tile))
+    # for line in tile[1]:
+    #     for c in line:
+    #         print(c, end='')
+    #     print()
     grid = tile[1]
     new_grid = []
     for line in reversed(grid):
@@ -67,10 +68,10 @@ def flipY(tile):
     new_links[1].append(links[1][3])
     tile[2] = new_links
     print("flippedy: " + str(tile))
-    for line in tile[1]:
-        for c in line:
-            print(c, end='')
-        print()
+    # for line in tile[1]:
+    #     for c in line:
+    #         print(c, end='')
+    #     print()
     return tile
 
 
@@ -99,10 +100,10 @@ def flipX(tile):
     new_links[1].append(links[1][1])
     tile[2] = new_links
     print("flippedx: ")
-    for line in tile[1]:
-        for c in line:
-            print(c, end='')
-        print()
+    # for line in tile[1]:
+    #     for c in line:
+    #         print(c, end='')
+    #     print()
     return tile
 
 
@@ -132,7 +133,7 @@ def load(filename):
     return tiles
 
 
-data = load("day20test.txt")
+data = load("day20.txt")
 
 for tile in data:
     # print(tile[0])
@@ -152,8 +153,9 @@ for tile in data:
     if count == 2:
         prod *= int(tile[0])
         print(tile)
-        if tile[0] == '1951':
-            corner = flipY(tile)
+        # if tile[0] == '1951':
+        #     corner = flipY(tile)
+        corner = tile
 print('Answer part 1: ' + str(prod))
 
 final_grid = []
@@ -183,16 +185,16 @@ print("Rotated corner: " + str(corner))
 
 current_tile = corner
 vert_pos = 0
-for y in range(2 + 1):
-    print(str(current_tile))
-    for x in range(len(current_tile[1])):
-        if 0 < x < len(current_tile[1]) - 1:
-            final_grid.append(current_tile[1][x][1:-1])
+for y in range(12):
+    print("row start: " + str(current_tile))
+    for z in range(len(current_tile[1])):
+        if 0 < z < len(current_tile[1]) - 1:
+            final_grid.append(current_tile[1][z][1:-1])
     line_tile = current_tile.copy()
-    for x in range(2):
+    for x in range(11):
         for match in data:
             if line_tile[2][1][1] == match[0]:
-                #print(match)
+                print(match)
                 next_tile = match
                 while next_tile[2][1][3] != line_tile[0]:
                     #print("rotate..." + str(next_tile[0]))
@@ -200,11 +202,11 @@ for y in range(2 + 1):
 
         if next_tile[2][0][3] == line_tile[2][0][1]:
             next_tile = flipY(next_tile)
-        print(str(next_tile))
-        for x in range(len(next_tile[1])):
+        print("collumn continue: " + str(x) + str(y) +"  " + str(next_tile))
+        for xx in range(len(next_tile[1])):
             r = len(next_tile[1]) - 2
-            if 0 < x < len(next_tile[1]) - 1:
-                final_grid[y*r+x-1] = final_grid[y*r+x-1] + next_tile[1][x][1:-1]
+            if 0 < xx < len(next_tile[1]) - 1:
+                final_grid[y*r+xx-1] = final_grid[y*r+xx-1] + next_tile[1][xx][1:-1]
         line_tile = next_tile
     #print(str(current_tile))
     for match2 in data:
@@ -231,6 +233,131 @@ for line in grid_envelope[1]:
     for n in line:
         print(n, end='')
     print()
+
+
+def checkForMonster(grid):
+    count = 0
+    #print(len(grid))
+    for x in range(len(grid[0]) - 18):#20):
+        for y in range(len(grid) - 1):
+
+            if grid[y][x + 18] == "#" and \
+                grid[y + 1][x] == "#" and \
+                grid[y + 1][x + 5] == "#" and \
+                grid[y + 1][x + 6] == "#" and \
+                grid[y + 1][x + 11] == "#" and \
+                grid[y + 1][x + 12] == "#" and \
+                grid[y + 1][x + 17] == "#" and \
+                grid[y + 1][x + 18] == "#" and \
+                grid[y + 1][x + 19] == "#" and \
+                grid[y + 2][x+1] == "#" and \
+                grid[y + 2][x + 4] == "#" and \
+                grid[y + 2][x + 7] == "#" and \
+                grid[y + 2][x + 10] == "#" and \
+                grid[y + 2][x + 13] == "#" and \
+                grid[y + 2][x + 16] == "#":
+                grid[y][x + 18] = '0'
+                grid[y + 1][x] = '0'
+                grid[y + 1][x + 5] = '0'
+                grid[y + 1][x + 6] = '0'
+                grid[y + 1][x + 11] = '0'
+                grid[y + 1][x + 12] = '0'
+                grid[y + 1][x + 17] = '0'
+                grid[y + 1][x + 18] = '0'
+                grid[y + 1][x + 19] = '0'
+                grid[y + 2][x + 1] = '0'
+                grid[y + 2][x + 4] = '0'
+                grid[y + 2][x + 7] = '0'
+                grid[y + 2][x + 10] = '0'
+                grid[y + 2][x + 13] = '0'
+                grid[y + 2][x + 16] = '0'
+                
+                count += 1
+
+    if count > 0:
+        print(str(count) +" Monster(s) found")
+    else:
+        print("-")
+    return grid,count
+
+print()
+print()
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+
+grid_envelope = flipX(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]      #41 found
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+
+grid_envelope = flipX(grid_envelope)
+grid_envelope = flipY(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]    #41 found
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+
+grid_envelope = flipX(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+
+grid_envelope = flipY(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]    #41 found
+grid_envelope = rotate(grid_envelope)
+grid_envelope[1] = checkForMonster(grid_envelope[1])[0]
+grid_envelope = rotate(grid_envelope)
+
+print()
+print()
+print()
+print()
+count = 0
+totlcount = 0
+monster_count = 0
+empty_count = 0
+for line in grid_envelope[1]:
+    for n in line:
+        totlcount += 1
+        if n == '#':
+            count += 1
+            print(',', end='')
+        elif n == '0':
+            monster_count += 1
+            print(n, end='')
+        else:
+            empty_count += 1
+            print(n, end='')
+    print()
+print("Answer part 2: " +str(count))
+print(totlcount)
+print(monster_count)
+print(empty_count)
+
 
 
 
